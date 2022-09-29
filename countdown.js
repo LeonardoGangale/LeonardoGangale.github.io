4ca710876d78c7124809bc771b257eecf7644f42
Leonardo Gangale <61387277+LeonardoGangale@users.noreply.github.com>
2022-09-29 19:43:30 +0200

    Add files via upload

--- countdown.js
+++ countdown.js
@@ -3,10 +3,40 @@ const text_element = document.getElementById("countdown_text")
 // Set the date we're counting down to
 var countDownDate = new Date("Oct 5, 2022 7:00:00").getTime();
 
+var xmlHttp;
+function srvTime(){
+    try {
+        //FF, Opera, Safari, Chrome
+        xmlHttp = new XMLHttpRequest();
+    }
+    catch (err1) {
+        //IE
+        try {
+            xmlHttp = new ActiveXObject('Msxml2.XMLHTTP');
+        }
+        catch (err2) {
+            try {
+                xmlHttp = new ActiveXObject('Microsoft.XMLHTTP');
+            }
+            catch (eerr3) {
+                //AJAX not supported, use CPU time.
+                alert("AJAX not supported");
+            }
+        }
+    }
+    xmlHttp.open('HEAD',window.location.href.toString(),false);
+    xmlHttp.setRequestHeader("Content-Type", "text/html");
+    xmlHttp.send('');
+    return xmlHttp.getResponseHeader("Date");
+}
+
+
+
 // Update the count down every 1 second
 var x = setInterval(function () {
   // Get today's date and time
-  var now = new Date().getTime();
+  var st = srvTime();
+  var now = new Date(st);
 
   // Find the distance between now and the count down date
   var distance = countDownDate - now;
@@ -26,6 +56,6 @@ var x = setInterval(function () {
     clearInterval(x);
     text_element.style.bottom = "13%"
     text_element.style.fontSize = "4vw"
-    text_element.innerHTML = "<a href='https://shorturl.at/fhivw' >???</a>"
+    text_element.innerHTML = "<a href='https://youtu.be/8FIpDIMjSTs' >???</a>"
   }
 }, 1000);
